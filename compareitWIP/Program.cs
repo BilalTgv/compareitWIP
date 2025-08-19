2e8536ed07a169f49f52932b8fdbcede0cc805d8
Bilal <bilal@bilal>
2025-08-19 08:57:53 +0200

    bleh

--- compareitWIP/Program.cs
+++ compareitWIP/Program.cs
@@ -7,10 +7,8 @@ namespace compareitWIP
 {
     public class MainForm : Form
     {
-
-        //hasushauhasuhasuas
-        private TextBox txtPath1;
-        private TextBox txtPath2;
+        private ComboBox cmbPath1;
+        private ComboBox cmbPath2;
         private Button btnBrowse1;
         private Button btnBrowse2;
         private Button btnCompare;
